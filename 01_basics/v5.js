2a12e218f5470958d2654082049aca537619d776
naveen02y <institutemanipal22@gmail.com>
2025-06-13 05:46:10 +0000

    data types and ecma standars

---
+++ 01_basics/v5.js
@@ -0,0 +1,14 @@
+"use strict"   //treat all js code as newer version
+// eppudu eythey use cheyani avarsaram kledhu
+
+
+//code should be readable
+/* number => 2^53/52
+bigint =>for llarge numbers
+string =>""
+boolean => True/False
+null =>standalone value  type of object
+undefined
+symbol =>unique
+*/
+
