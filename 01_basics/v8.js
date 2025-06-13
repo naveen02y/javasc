f8bb9e1ee4073ccf3122ef1b5f6901499b7413a3
naveen02y <institutemanipal22@gmail.com>
2025-06-13 10:58:17 +0000

    comparision of data types in js

---
+++ 01_basics/v8.js
@@ -0,0 +1,24 @@
+console.log(1==2)
+console.log(1>2)
+console.log(1>=2)
+// above are ok 
+// because comapring same data types
+
+console.log("1">2)// true ,but avoid like comapre same data type
+console.log(null > 0) // false ,js set null to 0
+console.log(null >= 0) // true, js set null to 0
+console.log(null ==0) //false
+
+
+console.log(undefined == 0) //f
+console.log(undefined >= 0)//f
+console.log(undefined > 0) //f
+
+
+// when using triple = then
+//there it compare datatype equal or not
+
+
+
+
+
