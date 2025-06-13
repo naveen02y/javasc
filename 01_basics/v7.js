3fb0a28afa575f211bd28248abdbfd02b5956b3b
naveen02y <institutemanipal22@gmail.com>
2025-06-13 07:55:09 +0000

    string to number conversion is confusion

---
+++ 01_basics/v7.js
@@ -0,0 +1,27 @@
+// mdn prefix and post fix
+console.log("1"+"2");//12
+console.log("1"+2)// 12
+console.log(1+"2") //12
+console.log("1"+2+2) //122
+console.log(1+2 +"2") //32
+
+//special type
+console.log(true) //true
+console.log (+true) // 1
+//console.log (true+) // error
+console.log (+"") //0
+
+let n1,n2,n3
+ // n1 = n2= n3 = 2+2
+//good but no readability
+ let gc = 100;
+ gc ++
+ console.log(gc)
+ ++gc
+ console.log(gc)
+
+
+ // js mdn  to read the document inc,dec 
+ //postfix ,prifix js mdn
+
+
