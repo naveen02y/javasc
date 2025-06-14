91f939e57837c4fc8122a41fc49cc0db121a6e9c
naveen02y <institutemanipal22@gmail.com>
2025-06-14 16:15:40 +0000

    array js

---
+++ 02_basics/array1.js
@@ -0,0 +1,12 @@
+const myarray = [1,2,3,4,5]
+
+//mix ok
+// not fixed length - aarray we can add
+  
+console.log(myarray[0])  //ok  
+// but console.log(myarray["one"]) // not accetable like in any language
+
+// shallow copy  - arry copy operations 
+// mean if array elememnts chnaged then orginal array elemenmts also changed 
+
+console.log("A",myarray)
