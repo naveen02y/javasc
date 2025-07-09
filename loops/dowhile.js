435105150bb69f4b77ef401d3bb865459b06acff
naveen02y <institutemanipal22@gmail.com>
2025-07-09 11:47:29 +0000

    do while loops

---
+++ loops/dowhile.js
@@ -0,0 +1,21 @@
+// here upto 10 score is ok
+let score =1;
+do{
+    //console.log(`score is${score}`)
+    score++
+}while(score<=10)
+
+// even use any higher value than 10 
+// one time the loop runs 
+// so we get 11
+
+let score1 =11;
+do{
+    console.log(`score is${score1}`)
+    score1++
+}while(score1<=10)
+ 
+
+
+
+
---
+++ loops/while.js
@@ -0,0 +1,13 @@
+let i =0;
+while(i <= 10){
+    //
+    // console.log(i)
+    i = i+2;
+}
+
+let n =0;
+let a = ["nav","man","uva"]
+while(n< a.length){
+    console.log(a[n]);
+    n = n+1;
+}
